82c4591a107eab75aa81670d744e93d2548ebbfe
Helena Cotterill <helena.cotterill@physics.ox.ac.uk>
2017-09-14 15:47:43 +0100

    final things

--- numerical_methods/diffusion_equation.py
+++ numerical_methods/diffusion_equation.py
@@ -5,7 +5,7 @@ import myfunctions as fn
 
 #set up x points, initial phi, courant number etc
 nx = 100    #no. x points
-nt = 2000  #no. timesteps
+nt = 100  #no. timesteps
 x=np.linspace(0.0, 1.0, nx+1) #setting up x values from 0-1 inclusive
 
 #A=fn.coefficient(nx, 0.00002, 0.5)
@@ -21,14 +21,14 @@ C=fn.diffusion_FTCS(B, nx, nt, A)
 
 D=fn.diffusion_FTCS(S, nx, nt, A)
 
-#E=fn.diffusion_CTCS(B, nx, nt, A)
+E=fn.diffusion_CTCS(B, nx, nt, A)
 
 #plot solution and compare to analytic solution (Bell curve)
 plt.plot(x, fn.initialBell(x), 'k', label='bell curve')
-plt.plot(x, fn.initialSquare(x), 'G', label='square')
+#plt.plot(x, fn.initialSquare(x), 'G', label='square')
 plt.plot(x, C, 'B', label='FTCS bell curve')
-plt.plot(x, D, 'R', label='FTCS square')
-#plt.plot(x, E, 'C', label='CTCS bell curve')
+#plt.plot(x, D, 'R', label='FTCS square')
+plt.plot(x, E, 'C', label='CTCS bell curve')
 plt.legend(loc='best')
 plt.xlabel('x')
 plt.ylabel('$\phi$')
